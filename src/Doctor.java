27c42671f1c5c86ebb34fa859374149da33c9a12
EnriqueGal <78947141+EnriqueGal@users.noreply.github.com>
2021-10-09 00:52:07 -0500

    Create Doctor.java

---
+++ src/Doctor.java
@@ -0,0 +1,34 @@
+public class Doctor {
+
+    private int idDoctor;
+    private String nombre;
+    private String apellido;
+
+    public int getIdDoctor() {
+        return idDoctor;
+    }
+
+    public void setIdDoctor(int idDoctor) {
+        this.idDoctor = idDoctor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String toString() {
+        return nombre + " " +apellido ;
+    }
+}
