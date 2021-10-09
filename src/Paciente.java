8ce1e274efb9d56c8a0fbdab2351fd50ee83e545
EnriqueGal <78947141+EnriqueGal@users.noreply.github.com>
2021-10-09 00:48:08 -0500

    Create Paciente.java

---
+++ src/Paciente.java
@@ -0,0 +1,34 @@
+public class Paciente {
+
+    private int idPaciente;
+    private String nombre;
+    private String apellido;
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String toString(){
+        return nombre + " " + apellido;
+    }
+}
