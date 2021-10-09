30d39203c7b7edc68533f5d62042de7c6459ccb1
EnriqueGal <78947141+EnriqueGal@users.noreply.github.com>
2021-10-09 00:59:57 -0500

    Create Citas.java

---
+++ src/Citas.java
@@ -0,0 +1,39 @@
+public class Citas {
+
+    private int idCita;
+    private String fecha;
+    private int idDoctor;
+    private int idPaciente;
+
+    public int getIdCita() {
+        return idCita;
+    }
+
+    public void setIdCita(int idCita) {
+        this.idCita = idCita;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getIdDoctor() {
+        return idDoctor;
+    }
+
+    public void setIdDoctor(int idDoctor) {
+        this.idDoctor = idDoctor;
+    }
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+}
